72ed1fbcd80cebf23857c69e8c97abc96196b714
Emre Karagozoglu <emrekaragozoglu@gmail.com>
2019-10-18 00:16:19 +0200

    adding the files and shell scripts

---
+++ files/correct_py2p_base_py.sh
@@ -0,0 +1,40 @@
+# INTEGRRATION CLUSTER
+
+# master
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.7//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# slave1
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.141.61//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.6/site-packages/py2p/
+
+# slave2
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.99//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# slave3
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.141.132//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.6/site-packages/py2p/
+
+# cassandra
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.184//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.6/site-packages/py2p/
+
+# cassandra-2
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.239//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.6/site-packages/py2p/
+
+
+# # PRODUCTION CLUSTER
+
+# # master
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.9.134//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# # slave1
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.8.200//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# # slave2
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.9.83//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# # slave3
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.8.239//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# # slave4
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.8.40//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
+
+# # slave5
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa base.py passhport@10.15.0.30:10.230.9.53//opt/apps/pop-blockchain/pop-blockchain-py3/lib/python3.4/site-packages/py2p/
---
+++ files/upload.sh
@@ -0,0 +1,40 @@
+# INTEGRRATION CLUSTER
+
+# master
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.7//opt/apps/pop-blockchain/
+
+# slave1
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.141.61//opt/apps/pop-blockchain/
+
+# slave2
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.99//opt/apps/pop-blockchain/
+
+# slave3
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.141.132//opt/apps/pop-blockchain/
+
+# cassandra
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.184//opt/apps/pop-blockchain/
+
+# cassandra-2
+scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.140.239//opt/apps/pop-blockchain/
+
+
+# # PRODUCTION CLUSTER
+
+# # master
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.9.134//opt/apps/pop-blockchain/
+
+# # slave1
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.8.200//opt/apps/pop-blockchain/
+
+# # slave2
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.9.83//opt/apps/pop-blockchain/
+
+# # slave3
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.8.239//opt/apps/pop-blockchain/
+
+# # slave4
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.8.40//opt/apps/pop-blockchain/
+
+# # slave5
+# scp -i /home/karagozoglu/karagozoglu@fr.jcdecaux.org.rsa $1 passhport@10.15.0.30:10.230.9.53//opt/apps/pop-blockchain/
